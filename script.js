4168fa98653cb786c54677f37a2328c7b1951399
hiphop200199 <hiphop200199@gmail.com>
2025-01-29 02:28:58 +0800

    s

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ window.addEventListener('load',function(){
     let close = document.getElementById('close')
     let slideLock = document.getElementById('slide-lock')
     let lockImg = document.getElementById('lock-img')
+    let grid = document.getElementById('grid')
+    let svgPath = document.querySelector('svg')
+    let message = document.getElementById('message')
     info.addEventListener('click',function(){
         let info = this
         info.innerText = ''
@@ -22,20 +25,47 @@ window.addEventListener('load',function(){
         upperCover.classList.add('open')
         upperInner.classList.add('open')
         lower.classList.add('open')
-     
+        lockImg.classList.remove('hide')
+        slideLock.classList.remove('hide')
+        slideLock.value = 0
+        message.innerText = 'Unlock.'
+        message.classList.add('show')
     })
     close.addEventListener('click',function(){
         phone.classList.remove('open')
         upperCover.classList.remove('open')
         upperInner.classList.remove('open')
         lower.classList.remove('open')
-    
+        grid.classList.remove('show')
+        svgPath.classList.remove('show')
+        message.classList.remove('show')
+        setTimeout(() => {
+            svgPath.style.display = 'none'
+            lockImg.style.display = 'inline'
+            slideLock.style.display = 'inline'
+        }, 1000);
     })
     slideLock.addEventListener('change',function(){
         if(this.value !=1){
             this.value = 0
             lockImg.style.animation = 'none'
             setTimeout(() => { lockImg.style.animation='shake 1s linear 1 forwards'}, 1);
+            return
         }
+        lockImg.classList.add('hide')
+        this.classList.add('hide')
+        grid.classList.add('show')
+        message.classList.remove('show')
+        setTimeout(() => {
+            this.style.display = 'none'
+            lockImg.style.display = 'none'
+             svgPath.style.display = 'grid'
+           setTimeout(() => {
+            svgPath.classList.add('show')
+            message.innerText = 'Welcome.'
+            message.classList.add('show')
+            }, 1);
+        }, 1000);
+   
     })
 })
